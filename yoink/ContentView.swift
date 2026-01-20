f65aabd7fd08a0310dab50ae46b6f5b8d11fe3c0
PopAV <a.popov@bssys.com>
2026-01-20 21:03:42 +0300

    Initial Commit

---
+++ yoink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yoink
+//
+//  Created by user on 20.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yoink/yoinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  yoinkApp.swift
+//  yoink
+//
+//  Created by user on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct yoinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
